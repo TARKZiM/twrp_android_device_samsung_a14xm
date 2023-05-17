983279360e7ee8b2a8989d6627a8daba9020f2fa
TARKZiM <tom8476oo@gmail.com>
2023-05-18 02:02:13 +0800

    a14xm: Update for A12.1 tree
    * Fixed cutout.
    * Added stock usb init rc.
    * Added reboot to download option.
    * Fixed MTP.
    * Fixed SDCard on data.

---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/a14xm
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vbmeta.img:$(PRODUCT_OUT)/vbmeta.img
