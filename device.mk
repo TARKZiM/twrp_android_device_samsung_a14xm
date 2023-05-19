5037ae754f8ec69aa78fbbc7734a6a55a50d5e7b
TARKZiM <tom8476oo@gmail.com>
2023-05-20 03:18:09 +0800

    a14xm: Support Dynamic partitions
    * Support metadata partition.
    * Add fastbootd.

--- device.mk
+++ device.mk
@@ -6,3 +6,11 @@
 #
 
 LOCAL_PATH := device/samsung/a14xm
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
