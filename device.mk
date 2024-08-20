a8eab7aa73f33871caebd2b8a43a063b8771e63a
TARKZiM <tom8476oo@gmail.com>
2024-08-21 03:45:55 +0800

    a14xm: Initial decryption support

--- device.mk
+++ device.mk
@@ -12,5 +12,16 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4 \
+    android.hardware.keymaster@4.1 \
+    android.hardware.security.keymint \
+    android.hardware.security.secureclock \
+    android.hardware.security.sharedsecret \
+    android.system.keystore2 \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery \
     android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
